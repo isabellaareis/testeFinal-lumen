38603a4c9632940a28a04bd3ac5aca128f85099f
Isabella Cristina Reis De Arruda <isabellaarruda69@gmail.com>
2025-06-19 15:42:27 -0300

    quinto commit

---
+++ assets/js/script.js
@@ -0,0 +1,18 @@
+/* MENU HAMBURGUER */
+
+const hamburger = document.getElementById("hamburger");
+const navMobile = document.getElementById("navMobile");
+
+hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle("active");
+    navMobile.classList.toggle("active");
+});
+
+/* FAQ */
+const faqItems = document.querySelectorAll('.faq-item');
+    faqItems.forEach(item => {
+        const question = item.querySelector('.faq-question');
+        question.addEventListener('click', () => {
+        item.classList.toggle('open');
+        });
+});
